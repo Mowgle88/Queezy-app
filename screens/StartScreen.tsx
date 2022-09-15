3761f07771b37e0612d7ed5c8a264774f27c41e9
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-15 22:40:59 +0300

    feat: add basic screens

---
+++ screens/StartScreen.tsx
@@ -0,0 +1,13 @@
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function StartScreen() {
+  return (
+    <View>
+      <Text>StartScreen</Text>
+      <Button title='signUp'></Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
