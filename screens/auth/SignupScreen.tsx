e4f690a035b922fe6d9d1e1aa5af81811a2a12f2
Mowgle88 <mowgle1988@gmail.com>
2022-09-20 00:06:45 +0300

    feat: add LoadingOverlay component

--- screens/auth/SignupScreen.tsx
+++ screens/auth/SignupScreen.tsx
@@ -2,13 +2,20 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { useState } from 'react'
 import AuthContent from '../../components/auth/AuthContent'
 import { createUser } from '../../util/auth'
+import LoadingOverlay from '../../components/ui/LoadingOverlay';
 
 export default function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
 
   async function signupHandler({ email, password }: { email: string, password: string }) {
+    setIsAuthenticating(true);
     createUser(email, password);
+    setIsAuthenticating(false);
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message={'Creating user...'} />
   }
 
   return (
