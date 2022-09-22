89260b26cc04108ce14d8a46628a5ce63676d8ec
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-22 22:50:31 +0300

    feat: add screens to app

---
+++ screens/main/AchievementsBoard.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AchievementsBoard() {
+  return (
+    <View>
+      <Text>AchievementsBoard</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
