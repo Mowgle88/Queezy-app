d19d43a7c0ca43005532aa35b487c9b2b7ff23b4
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-23 23:02:17 +0300

    feat: add TabBarIcon component, take out styles

--- navigation/MainTabs.tsx
+++ navigation/MainTabs.tsx
@@ -11,6 +11,7 @@ import AchievementsBoardScreen from '../screens/main/AchievementsBoardScreen';
 import ProfileScreen from '../screens/main/ProfileScreen';
 import { MainStackParamList } from './types';
 import { Colors } from '../constants/styles';
+import TabBarIcon from '../components/ui/TabBarIcon';
 
 const MainTab = createBottomTabNavigator<MainStackParamList>();
 
@@ -22,21 +23,10 @@ interface CustomTabBarButtonProps {
 function CustomTabBarButton({ children, onPress }: CustomTabBarButtonProps) {
   return (
     <TouchableOpacity
-      style={{
-        bottom: 60,
-        justifyContent: 'center',
-        alignItems: 'center',
-        ...styles.shadow
-      }}
+      style={[styles.customTabBarButtonContainer, styles.shadow]}
       onPress={onPress}
     >
-      <View style={{
-        width: 50,
-        height: 50,
-        borderRadius: 25,
-        backgroundColor: Colors.royalBlue
-      }}
-      >
+      <View style={styles.customTabBarButtonInnerContainer}>
         {children}
       </View>
     </TouchableOpacity>
@@ -50,7 +40,14 @@ export default function MainTabs() {
   return (
     <MainTab.Navigator
       screenOptions={{
-        headerRight: ({ tintColor }) => <IconButton icon={'exit'} size={24} color={tintColor!} onPress={authCtx.logout} />,
+        headerRight: ({ tintColor }) => (
+          <IconButton
+            icon={'exit'}
+            size={24}
+            color={tintColor!}
+            onPress={authCtx.logout}
+          />
+        ),
         tabBarShowLabel: false,
         tabBarStyle: {
           position: 'absolute',
@@ -63,51 +60,24 @@ export default function MainTabs() {
           <Image
             source={require('../assets/Bottom-tabs.png')}
             resizeMode='cover'
-            style={{
-              position: 'absolute',
-              bottom: 0,
-              width: '100%',
-            }}
+            style={styles.tabBarImageBackground}
           />
         ),
       }}
     >
       <MainTab.Screen name="Home" component={HomeScreen} options={{
         tabBarIcon: ({ focused }) => (
-          <Image
-            source={require('../assets/Home.png')}
-            resizeMode="contain"
-            style={{
-              width: focused ? 35 : 25,
-              height: focused ? 35 : 25,
-              tintColor: focused ? Colors.royalBlue : Colors.grey2
-            }}
-          />
+          <TabBarIcon focused={focused} source={require('../assets/Home.png')} />
         )
       }} />
       <MainTab.Screen name="Search" component={SearchScreen} options={{
         tabBarIcon: ({ focused }) => (
-          <Image
-            source={require('../assets/Search.png')}
-            resizeMode="contain"
-            style={{
-              width: focused ? 35 : 25,
-              height: focused ? 35 : 25,
-              tintColor: focused ? Colors.royalBlue : Colors.grey2
-            }}
-          />
+          <TabBarIcon focused={focused} source={require('../assets/Search.png')} />
         )
       }} />
       <MainTab.Screen name="CreateQuiz" component={CreateQuizScreen} options={{
         tabBarIcon: ({ focused }) => (
-          <Image
-            source={require('../assets/Plus.png')}
-            resizeMode="contain"
-            style={{
-              width: 125,
-              height: 125,
-            }}
-          />
+          <TabBarIcon isCustom style={styles.customImage} source={require('../assets/Plus.png')} />
         ),
         tabBarButton: (props) => (
           <CustomTabBarButton {...props} />
@@ -115,28 +85,12 @@ export default function MainTabs() {
       }} />
       <MainTab.Screen name="Achievements" component={AchievementsBoardScreen} options={{
         tabBarIcon: ({ focused }) => (
-          <Image
-            source={require('../assets/Achievements.png')}
-            resizeMode="contain"
-            style={{
-              width: focused ? 35 : 25,
-              height: focused ? 35 : 25,
-              tintColor: focused ? Colors.royalBlue : Colors.grey2
-            }}
-          />
+          <TabBarIcon focused={focused} source={require('../assets/Achievements.png')} />
         )
       }} />
       <MainTab.Screen name="Profile" component={ProfileScreen} options={{
         tabBarIcon: ({ focused }) => (
-          <Image
-            source={require('../assets/Profile.png')}
-            resizeMode="contain"
-            style={{
-              width: focused ? 35 : 25,
-              height: focused ? 35 : 25,
-              tintColor: focused ? Colors.royalBlue : Colors.grey2
-            }}
-          />
+          <TabBarIcon focused={focused} source={require('../assets/Profile.png')} />
         )
       }} />
     </MainTab.Navigator>
@@ -144,11 +98,31 @@ export default function MainTabs() {
 }
 
 const styles = StyleSheet.create({
+  customTabBarButtonContainer: {
+    bottom: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  customTabBarButtonInnerContainer: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: Colors.royalBlue
+  },
   shadow: {
     shadowColor: 'black',
     shadowOffset: { width: 0, height: 0 },
     shadowOpacity: 0.25,
     shadowRadius: 3.5,
     elevation: 3
+  },
+  customImage: {
+    width: 125,
+    height: 125,
+  },
+  tabBarImageBackground: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
   }
 })
