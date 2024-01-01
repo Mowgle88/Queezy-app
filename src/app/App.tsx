e805bf458d38af5210adff259ba3a40233bc2f4c
Dmitry Ilyin <mowgle1988@gmail.com>
2024-01-01 19:18:51 +0300

    feat: configure store

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,36 @@
 import React from "react";
 import { StatusBar, StyleSheet } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { Root } from "#navigation";
 import {
   AuthContextProvider,
   QuizContextProvider,
   UserContextProvider,
+  persistor,
+  store,
 } from "../store";
-import { SafeAreaProvider } from "react-native-safe-area-context";
 
 const App: React.FC = () => {
   return (
     <>
       <SafeAreaProvider>
         <StatusBar />
-        <AuthContextProvider>
-          <UserContextProvider>
-            <QuizContextProvider>
-              <GestureHandlerRootView style={styles.container}>
-                <Root />
-              </GestureHandlerRootView>
-            </QuizContextProvider>
-          </UserContextProvider>
-        </AuthContextProvider>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <AuthContextProvider>
+              <UserContextProvider>
+                <QuizContextProvider>
+                  <GestureHandlerRootView style={styles.container}>
+                    <Root />
+                  </GestureHandlerRootView>
+                </QuizContextProvider>
+              </UserContextProvider>
+            </AuthContextProvider>
+          </PersistGate>
+        </Provider>
       </SafeAreaProvider>
     </>
   );
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,42 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import {
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+  persistReducer,
+  persistStore,
+} from "redux-persist";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const persistConfig = {
+  storage: AsyncStorage,
+  key: "root",
+};
+
+const rootReducer = combineReducers({});
+
+export const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  devTools: true,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+      thunk: true,
+    }),
+});
+
+export const persistor = persistStore(store);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+
 export {
   type IAuthContext,
   AuthContextProvider,
